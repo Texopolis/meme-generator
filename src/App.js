fc18abf76dbc41f769220a791658a3ac612bc9d1
Douglas B <guod1977@gmail.com>
2022-02-01 18:40:40 -0800

    2-1 update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Header from './components/header'
 import Meme from './components/meme'
-import MemeContent from './components/memeContent'
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
         <Header />
         <div className="content">
           <Meme />
-          <MemeContent />
         </div>
       </div>
     </div>
